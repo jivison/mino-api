97ba30616804afce41af4938626b60a74d49fcc5
John Ivison <john@ivison.org>
2019-10-30 13:14:41 -0700

    Add Readme + logo

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,9 +2,14 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   # Disable postgres logging
-  ActiveRecord::Base.logger.level = 1 # or Logger::INFO
+  ActiveRecord::Base.logger.level = 1
 
   def self.group_by(column)
+    # returns:
+    # {
+    #   column_value1: [entity1, entity3],
+    #   column_value2: [entity2]
+    # }
     self.all.inject({}) do |acc, entity| 
       acc[entity.send(column)] ||= []
       acc[entity.send(column)] << entity
